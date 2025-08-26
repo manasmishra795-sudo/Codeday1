6721e1ad4e3e5a590c2dbd5b5eb4f10ab784f331
manasmishra795-sudo <manasmishra795@gmail.com>
2025-08-26 11:05:09 +0530

    Add files via upload

---
+++ positivenegative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+    int n;
+    printf("Enter the number:");
+    scanf("%d", &n);
+    if (n>0)
+    {
+        printf("The number is positive");
+    }
+    else if (n<0)
+    {
+        printf("The number is negative");
+    }
+    else{
+        printf("The number is 0");
+        return 0;
+
+    }
+}
