bec735f2b4b5f25e28ab60e0fc8d4d2fe4531b3a
manasmishra795-sudo <manasmishra795@gmail.com>
2025-09-16 18:21:48 +0530

    Add files via upload

---
+++ numberpattern2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int rows,num,i,space,number;
+    printf("Enter the number of rows:");
+    scanf("%d",&rows);
+    if(rows<=0)
+    {
+        printf("Enter a valid valuie.");
+        return 0;
+    }
+    for(i=0;i<rows;i++)
+    {
+        for(space=1;space<=rows-i;space++)
+        {
+            printf(" ");
+        }
+        number=rows-i;
+        for(num=0;num<=i;num++)
+        {
+            
+            printf("%d", number);
+            number++;
+            
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
