b8ceab6736f61824e7d0fb6ec2ec2e9492fc57c1
manasmishra795-sudo <manasmishra795@gmail.com>
2025-10-21 22:20:12 +0530

    Add files via upload

---
+++ halfdiamond.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main ()
+{
+    int rows,star,i;
+    printf("Enter the number of rows:");
+    scanf("%d",&rows);
+    if(rows<=0)
+    {
+        printf("Enter a valid value.");
+        return 0;
+    }
+    for(i=0;i<rows;i++)
+    {
+      for(star=0;star<=i;star++)
+      {
+        printf("*");
+      }
+      printf("\n");
+    }
+    for(i=rows-1;i>0;i--)
+    {
+        for(star=1;star<=i;star++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
