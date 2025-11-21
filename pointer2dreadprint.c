7e49facaa4ad1569447f01c215dcdac0f4866b65
manasmishra795-sudo <manasmishra795@gmail.com>
2025-11-21 21:27:52 +0530

    Add files via upload

---
+++ pointer2dreadprint.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+void inputrowcol(int row,int column)
+{
+    printf("Enter the rows and column of the matrix:\n");
+    scanf("%d %d",&row,&column);
+    if(row<0||row>100||column<=0||column>100)
+    {
+        printf("Invalid value.");
+        return;
+    }
+
+}
+void twoinput(int(*p)[100],int row,int column)
+{
+    int i,k;
+    for(i=0;i<row;i++)
+    {
+        for(k=0;k<column;k++)
+        {
+            scanf("%d",(*(p+i)+k));
+        }
+    }
+}
+void twooutput(int(*p)[100],int row,int column)
+{
+    int i,k;
+    for(i=0;i<row;i++)
+    {
+        for(k=0;k<column;k++)
+        {
+            printf("%4d",*(*(p+i)+k));
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int array[100][100],i,k,row,column;
+    int(*p)[100]=array;
+    printf("Enter the rows and column of the matrix:\n");
+    scanf("%d %d",&row,&column);
+    if(row<0||row>100||column<=0||column>100)
+    {
+        printf("Invalid value.");
+        return 0;
+    }
+    printf("Enter the elements in the matrix:\n");
+    for(i=0;i<row;i++)
+    {
+        for(k=0;k<column;k++)
+        {
+            scanf("%d",(*(p+i)+k));
+        }
+    }
+    printf("Matrix:\n");
+    for(i=0;i<row;i++)
+    {
+        for(k=0;k<column;k++)
+        {
+            printf("%4d",*(*(p+i)+k));
+        }
+        printf("\n");
+    }
+    return 0;
+
+
+
+}
